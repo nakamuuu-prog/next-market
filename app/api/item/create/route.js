967ce3aeb35dbef4770f78d385b0298cc25098f4
nakamuuu-prog <affilimoney2@gmail.com>
2024-07-08 08:56:46 +0900

    スキーマの設定、リクエストデータのDB保存

--- app/api/item/create/route.js
+++ app/api/item/create/route.js
@@ -1,11 +1,17 @@
 import { NextResponse } from "next/server";
 import connectDB from "@/app/utils/database";
+import { ItemModel } from "@/app/utils/schemaModels.";
 
 // メソッド名はCRUDに対応している
 // GET、POST、PUT、DELETEのメソッドを作成すると、それぞれのリクエストに対するレスポンスが返る
 export async function POST(repuest) {
-  // Thunder ClientのBodyに設定したJSONが表示される
-  console.log(await repuest.json());
-  connectDB();
-  return NextResponse.json({ message: "アイテム作成" });
+  const reqBody = await repuest.json();
+
+  try {
+    await connectDB();
+    await ItemModel.create(reqBody);
+    return NextResponse.json({ message: "アイテム作成成功" });
+  } catch (error) {
+    return NextResponse.json({ message: "アイテム作成失敗" });
+  }
 }
