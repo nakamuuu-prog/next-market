f869e590189a56262d44cbfc5dc77a3f3744d8cf
nakamuuu-prog <affilimoney2@gmail.com>
2024-07-11 09:14:37 +0900

    middlewareを使ってトークンが有効かを判定する

---
+++ middleware.js
@@ -0,0 +1,45 @@
+import { jwtVerify } from "jose";
+import { NextResponse } from "next/server";
+
+// middleware.jsはすべてのやりとり(request、response)の際に必ず経由される
+// => ファイル名はmiddleware2.jsにすると機能しなくなった
+export async function middleware(request) {
+  // ログイン時に発行されたトークンを使用してデコードされたトークンを確認できる
+  const token =
+    "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImR1b15QGdtYWlsLmNvbSIsImV4cCI6MTcyMDc0MjczMn0.ksSF5Z69ulmTsRp3hBOFDcdxzT8Zy713v_6yJRVdUFc";
+  // headerのlocal strageからトークンを取得する
+  // const token = await request.headers.get("Authorization")?.split(" ")[1];
+
+  if (!token) {
+    return NextResponse.json({ message: "トークンがありません" });
+  }
+  try {
+    const secretKey = new TextEncoder().encode("next-market-app-book");
+    // jwtVerifyを使ってトークンの判定を行う
+    // 不正なトークンであったり、有効期限が過ぎている場合はエラーになる
+    const decodedJwt = await jwtVerify(token, secretKey);
+    console.log("decodedJwt:", decodedJwt);
+    // 結果
+    // decodedJwt: {
+    //   payload: { email: 'dummy@gmail.com', exp: 1720742732 },
+    //   protectedHeader: { alg: 'HS256' }
+    // }
+
+    // NextResponse.nextはこのファイルの処理が問題なく完了したことを告げるコード
+    return NextResponse.next();
+  } catch (error) {
+    return NextResponse.json({
+      message: "トークンが正しくないので、ログインしてください",
+    });
+  }
+}
+
+// configを宣言し、matcherの配列に指定のパスを入れることで、middlewareを経由したい機能を制限できる
+// updateやdeleteはidによってパスが異なるため、「:path*」とすることで動的なパスに対応できる
+export const config = {
+  matcher: [
+    "/api/item/create",
+    "/api/item/update/:path*",
+    "/api/item/delete/:path*",
+  ],
+};
