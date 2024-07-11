5c79e1ca397eb3d964ef30e8ed719ffbd69bbe40
nakamuuu-prog <affilimoney2@gmail.com>
2024-07-11 23:05:22 +0900

    アイテムを作成したユーザーのみが更新、削除できるようにする

--- app/api/item/delete/[id]/route.js
+++ app/api/item/delete/[id]/route.js
@@ -5,8 +5,12 @@ import { NextResponse } from "next/server";
 export async function DELETE(request, context) {
   try {
     await connectDB();
-    await ItemModel.deleteOne({ _id: context.params.id });
-    return NextResponse.json({ message: "アイテム削除成功" });
+    const singleItem = await ItemModel.findById(context.params.id);
+    if (singleItem.email === reqBody.email) {
+      await ItemModel.deleteOne({ _id: context.params.id });
+      return NextResponse.json({ message: "アイテム削除成功" });
+    }
+    return NextResponse.json({ message: "他の人が作成したアイテムです" });
   } catch (error) {
     return NextResponse.json({ message: "アイテム削除失敗" });
   }
--- app/api/item/update/[id]/route.js
+++ app/api/item/update/[id]/route.js
@@ -4,12 +4,20 @@ import { NextResponse } from "next/server";
 
 export async function PUT(request, context) {
   const reqBody = await request.json();
+  console.log(reqBody);
   try {
     await connectDB();
-    // updateOneでMongoDBのデータを更新できる
-    // findByIdは_idを使うことを前提としているので、updateOneは_idが含まれたオブジェクトにプライマリキーを指定する必要がある
-    await ItemModel.updateOne({ _id: context.params.id }, reqBody);
-    return NextResponse.json({ message: "アイテム編集成功" });
+    const singleItem = await ItemModel.findById(context.params.id);
+    // DBのEmailとリクエストのEmailが一致したときだけ編集可能にする(削除も同じ仕組み)
+    // => ここってリクエストのEmailじゃなくてLocalStrageにあるトークンから判断するのが正しいのでは？
+    // => まだLocalStrageの実装はされていないので、一旦置いとく
+    if (singleItem.email === reqBody.email) {
+      // updateOneでMongoDBのデータを更新できる
+      // findByIdは_idを使うことを前提としているので、updateOneは_idが含まれたオブジェクトにプライマリキーを指定する必要がある
+      await ItemModel.updateOne({ _id: context.params.id }, reqBody);
+      return NextResponse.json({ message: "アイテム編集成功" });
+    }
+    return NextResponse.json({ message: "他の人が作成したアイテムです" });
   } catch (error) {
     // idが間違っているとちゃんと失敗する
     return NextResponse.json({ message: "アイテム編集失敗" });
