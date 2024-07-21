597f2c34fb42b76e1390c390861ac2d70ac11d73
nakamuuu-prog <affilimoney2@gmail.com>
2024-07-21 23:31:30 +0900

    アイテム一覧からアイテム個別ページに遷移可能にする

--- app/page.js
+++ app/page.js
@@ -20,6 +20,7 @@ const getAllItems = async () => {
 
 // 「async/awaitはクライアント・コンポーネントではまだサポートされていません」というエラーが起こる
 // Error: async/await is not yet supported in Client Components, only Server Components. This error is often caused by accidentally adding `'use client'` to a module that was originally written for the server.
+// => "use client"の削除漏れ
 const ReadAllItems = async () => {
   const allItems = await getAllItems();
   return (
@@ -27,7 +28,7 @@ const ReadAllItems = async () => {
       {/* 配列の中にアイテムが1つ1つオブジェクトとして入っているので取り出す */}
       {allItems.map((item) => (
         // 「Warning: Each child in a list should have a unique "key" prop.」というエラーを回避するために、1つ1つのデータにkeyを設定する必要がある
-        <Link href="" key={item._id}>
+        <Link href={`/item/readsingle/${item._id}`} key={item._id}>
           {/* Imageは必ずheightとwidthを指定しなければいけない */}
           <Image
             src={item.image}
